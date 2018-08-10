159331aa703d40fef6f42845dea3db93c80eec80
Julianna Tetreault <juliannatetreault@icloud.com>
2018-08-10 11:23:58 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+function paragraphSelector() {
+  $('p');
+}
+
+function lastImageSelector() {
+  $('img:last');
+}
+
+function ninjaBabySelector() {
+  $('#ninja-baby');
+}
+
+function divSelector() {
+  $('.pics');
+}
+
+function firstListItem() {
+  $('ul li:first-child');
+}
